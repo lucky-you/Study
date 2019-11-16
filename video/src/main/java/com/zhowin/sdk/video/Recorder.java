0eb883fcb5fb75b7c51b10879f4840c16e374453
Chvigteny <1021237228@qq.com>
2019-11-16 16:58:18 +0800

    使用ffmpeg录制视频

--- video/src/main/java/com/zhowin/sdk/video/Recorder.java
+++ video/src/main/java/com/zhowin/sdk/video/Recorder.java
@@ -46,6 +46,7 @@ public class Recorder implements Camera.PreviewCallback {
 
     /**
      * 获取视频路径
+     *
      * @return
      */
     public String getOutputPath() {
@@ -54,6 +55,7 @@ public class Recorder implements Camera.PreviewCallback {
 
     /**
      * 是否正在录制中
+     *
      * @return
      */
     public boolean isRecording() {
@@ -62,10 +64,11 @@ public class Recorder implements Camera.PreviewCallback {
 
     /**
      * 获取录制时长 单位ms
+     *
      * @return
      */
     public long getRecordingTime() {
-        if(mStartTime == 0) {
+        if (mStartTime == 0) {
             return 0;
         }
         return System.currentTimeMillis() - mStartTime;
@@ -73,6 +76,7 @@ public class Recorder implements Camera.PreviewCallback {
 
     /**
      * 切换前后摄像头
+     *
      * @param camera
      * @param face
      */
@@ -85,13 +89,11 @@ public class Recorder implements Camera.PreviewCallback {
     private void initRecorder() {
         Camera.Size previewSize = CameraHelper.getFullScreenPreviewSize(mBuilder.mContext, mBuilder.mCamera);
         mFrame = new Frame(previewSize.width, previewSize.height, Frame.DEPTH_UBYTE, 2);
-
         //提前加载2个framerecorder
         mFFmpegFrameRecorderV = new FFmpegFrameRecorder(mBuilder.mOutputFilePath, mBuilder.mOutputWidth, mBuilder.mOutputHeight, 1);
         initFrameRecorder(mFFmpegFrameRecorderV);
         mFFmpegFrameRecorderL = new FFmpegFrameRecorder(mBuilder.mOutputFilePath, mBuilder.mOutputHeight, mBuilder.mOutputWidth, 1);
         initFrameRecorder(mFFmpegFrameRecorderL);
-
         //音频
         mAudioRecordRunnable = new AudioRecordRunnable();
     }
@@ -102,7 +104,6 @@ public class Recorder implements Camera.PreviewCallback {
         frameRecorder.setVideoOption("preset", "ultrafast");        //加速
         frameRecorder.setFrameRate(frameRate);
         frameRecorder.setVideoQuality(0);
-
         frameRecorder.setAudioChannels(1);
         frameRecorder.setSampleRate(sampleAudioRateInHz);
     }
@@ -110,7 +111,6 @@ public class Recorder implements Camera.PreviewCallback {
     //初始化filter
     private void initFilter() {
         Camera.Size previewSize = CameraHelper.getFullScreenPreviewSize(mBuilder.mContext, mBuilder.mCamera);
-
         //根据定义的视频尺寸和预览的尺寸进行切割、旋转
         Float outputRatio = 1f * mBuilder.mOutputHeight / mBuilder.mOutputWidth;
         int cropWidth = (int) (outputRatio * previewSize.height);
@@ -120,15 +120,13 @@ public class Recorder implements Camera.PreviewCallback {
         int rotation = 90;      //顺时针90度
         boolean hflip = false;
         rotation = (rotation + mBuilder.mCameraOrientationDetector.getOrientation()) % 360;
-
         //如果是前置摄像头 改变旋转方向 修改裁剪位置 水平旋转
-        if(mBuilder.mCameraFace == Camera.CameraInfo.CAMERA_FACING_FRONT) {
+        if (mBuilder.mCameraFace == Camera.CameraInfo.CAMERA_FACING_FRONT) {
             startX = previewSize.width - cropWidth;
             rotation = 270;        //270度
             rotation = (rotation - mBuilder.mCameraOrientationDetector.getOrientation()) % 360;
             hflip = true;       //自拍水平旋转
         }
-
         String filters = generateFilters(cropWidth, cropHeight, startX, startY, rotation, hflip);
         mFFmpegFrameFilter = new FFmpegFrameFilter(filters, previewSize.width, previewSize.height);
         mFFmpegFrameFilter.setPixelFormat(org.bytedeco.javacpp.avutil.AV_PIX_FMT_NV21); // default camera format on Android
@@ -136,15 +134,16 @@ public class Recorder implements Camera.PreviewCallback {
 
     /**
      * 开始录制
+     *
      * @throws FrameRecorder.Exception
      * @throws FrameFilter.Exception
      */
     public void start() throws FrameRecorder.Exception, FrameFilter.Exception {
-        if(mRecording) {
+        if (mRecording) {
             return;
         }
         int rotation = mBuilder.mCameraOrientationDetector.getOrientation();
-        if(rotation == 0 || rotation == 180) {
+        if (rotation == 0 || rotation == 180) {
             mFFmpegFrameRecorder = mFFmpegFrameRecorderV;
         } else {
             mFFmpegFrameRecorder = mFFmpegFrameRecorderL;
@@ -162,14 +161,14 @@ public class Recorder implements Camera.PreviewCallback {
 
     /**
      * 暂停视频
+     *
      * @throws FrameRecorder.Exception
      * @throws FrameFilter.Exception
      */
     public void stop() throws FrameRecorder.Exception, FrameFilter.Exception {
-        if(!mRecording) {
+        if (!mRecording) {
             return;
         }
-
         mStartTime = 0L;
         mRunAudioThread = false;
         try {
@@ -180,7 +179,6 @@ public class Recorder implements Camera.PreviewCallback {
             return;
         }
         mAudioThread = null;
-
         if (mFFmpegFrameRecorder != null && mRecording) {
             mRecording = false;
             mFFmpegFrameRecorder.stop();
@@ -192,39 +190,35 @@ public class Recorder implements Camera.PreviewCallback {
 
     /**
      * 生成处理配置
-     * @param w 裁切宽度
-     * @param h 裁切高度
-     * @param x 裁切起始x坐标
-     * @param y 裁切起始y坐标
+     *
+     * @param w        裁切宽度
+     * @param h        裁切高度
+     * @param x        裁切起始x坐标
+     * @param y        裁切起始y坐标
      * @param rotation 图像旋转度数
-     * @param hflip 是否水平翻转
+     * @param hflip    是否水平翻转
      * @return 帧图像数据处理参数
      */
     private String generateFilters(int w, int h, int x, int y, int rotation, boolean hflip) {
         String filters = String.format("crop=w=%d:h=%d:x=%d:y=%d", w, h, x, y);
-
-        for(int i = 0; i < rotation/90; i ++) {
+        for (int i = 0; i < rotation / 90; i++) {
             filters += ",transpose=clock";
         }
-
-        if(hflip) {
+        if (hflip) {
             filters += ",hflip";
         }
-
         return filters;
     }
 
     @Override
     public void onPreviewFrame(byte[] data, Camera camera) {
         if (mFrame != null && mRecording) {
-            ((ByteBuffer)mFrame.image[0].position(0)).put(data);
-
+            ((ByteBuffer) mFrame.image[0].position(0)).put(data);
             try {
                 long t = 1000 * (System.currentTimeMillis() - mStartTime);
                 if (t > mFFmpegFrameRecorder.getTimestamp()) {
                     mFFmpegFrameRecorder.setTimestamp(t);
                 }
-
                 mFFmpegFrameFilter.push(mFrame);
                 Frame frame2;
                 while ((frame2 = mFFmpegFrameFilter.pull()) != null) {
@@ -291,20 +285,20 @@ public class Recorder implements Camera.PreviewCallback {
         }
 
         public Recorder build() throws IllegalArgumentException {
-            if(mContext == null) {
+            if (mContext == null) {
                 throw new IllegalArgumentException("mContext为空");
             }
-            if(mCamera == null) {
+            if (mCamera == null) {
                 throw new IllegalArgumentException("mCamera为空");
             }
-            if(mCameraFace != Camera.CameraInfo.CAMERA_FACING_BACK && mCameraFace != Camera.CameraInfo.CAMERA_FACING_FRONT) {
+            if (mCameraFace != Camera.CameraInfo.CAMERA_FACING_BACK && mCameraFace != Camera.CameraInfo.CAMERA_FACING_FRONT) {
                 throw new IllegalArgumentException("mCameraFace非法");
             }
             checkFilePath(mOutputFilePath);
-            if(mOutputWidth < 0 || mOutputHeight < 0) {
+            if (mOutputWidth < 0 || mOutputHeight < 0) {
                 throw new IllegalArgumentException("mOutputWidth mOutputHeight非法");
             }
-            if(mCameraOrientationDetector == null) {
+            if (mCameraOrientationDetector == null) {
                 throw new IllegalArgumentException("mMyOrientationDetector为空");
             }
             return new Recorder(this);
@@ -312,13 +306,13 @@ public class Recorder implements Camera.PreviewCallback {
 
         //检查filePath有效性
         private void checkFilePath(String filePath) throws IllegalArgumentException {
-            if(filePath.length() == 0) {
+            if (filePath.length() == 0) {
                 throw new IllegalArgumentException("mOutputFilePath为空");
             }
 
             File file = new File(filePath);
-            if(file.exists()) {
-                return ;
+            if (file.exists()) {
+                return;
             }
 
             if (filePath.endsWith(File.separator)) {
@@ -326,8 +320,8 @@ public class Recorder implements Camera.PreviewCallback {
             }
 
             //判断目标文件所在的目录是否存在
-            if(!file.getParentFile().exists()) {
-                if(!file.getParentFile().mkdirs()) {
+            if (!file.getParentFile().exists()) {
+                if (!file.getParentFile().mkdirs()) {
                     throw new IllegalArgumentException("创建目标文件所在目录失败！");
                 }
             }
@@ -364,7 +358,7 @@ public class Recorder implements Camera.PreviewCallback {
                 bufferReadResult = mAudioRecord.read(audioData.array(), 0, audioData.capacity());
                 audioData.limit(bufferReadResult);
                 if (bufferReadResult > 0) {
-                    if(mFFmpegFrameRecorder != null && mRecording) {
+                    if (mFFmpegFrameRecorder != null && mRecording) {
                         try {
                             mFFmpegFrameRecorder.recordSamples(audioData);      //写入音频数据
                         } catch (FFmpegFrameRecorder.Exception e) {
